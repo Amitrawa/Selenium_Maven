fd239951eccc2bb14b191f101f2de171108ef4df
amit <amitrawatddn@gmail.com>
2024-05-21 22:43:33 +0530

    fourth commit

--- src/test/java/testPrint.java
+++ src/test/java/testPrint.java
@@ -1,6 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class testPrint {
 
-    public static void main(String[] args) {
-        System.out.println("Print Something");
-    }
+    @Test
+    void openInstagram(){
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.instagram.com/");
+        driver.quit();
+
+  }
 }
